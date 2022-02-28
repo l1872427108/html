b23aecc545e1cc7fb249f78264eb22a272f764bd
rck <296046734@qq.com>
2022-02-28 21:55:13 +0800

    second

---
+++ docs/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "HTML",
+    base: '/html/',
+    themeConfig: {
+      nav: [{
+        text: 'Home', link: '/'
+      }, {
+        text: 'Github', link: 'https://github.com/l1872427108/uui'
+      }, {
+        text: 'Npm', link: 'https://www.npmjs.com/package/uu-uui'
+      }],
+      sidebar: [
+        '/',
+        '/componentDocs/meta'
+      ]
+    }
+  }
+  
